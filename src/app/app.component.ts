72615843a0bb4c26feceea8b92830c1c038bab03
Miljan Lazic <miljanlazic@gmail.com>
2024-04-02 17:38:26 +0200

    Last commit on standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from '../components/login/login.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  imports: [RouterOutlet, LoginComponent],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Coffe.QR-FrontEnd';
--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
 
+  goToRegister() {
+    this.router.navigate(['/register']); // Use the class property here
+  }
 }
