76bbfa4ef48336dc410ee769746101a2a9bf6a82
z1 <miljanlazic@gmail.com>
2024-04-08 18:48:28 +0200

    FEAT: NavBar update

--- src/components/Xuniversal/navbar/navbar.component.ts
+++ src/components/Xuniversal/navbar/navbar.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { AuthService } from '../../../auth/auth.service'
+import { User } from '../../../auth/model/user.model'
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss',
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-  navbarOpen = false;
+export class NavbarComponent implements OnInit {
+    navbarOpen = false
+    user: User | undefined
+    isDropdownOpen: boolean = false
 
-  toggleNavbar() {
-    this.navbarOpen = !this.navbarOpen;
-  }
+    constructor(private authService: AuthService) {}
+
+    toggleDropdown(event: Event) {
+        event.stopPropagation()
+        this.isDropdownOpen = !this.isDropdownOpen
+    }
+
+    ngOnInit(): void {
+        this.authService.user$.subscribe((user) => {
+            this.user = user
+        })
+    }
+
+    toggleNavbar() {
+        this.navbarOpen = !this.navbarOpen
+    }
+
+    logout() {
+        this.authService.logout()
+    }
 }
