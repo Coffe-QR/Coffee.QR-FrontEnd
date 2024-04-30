c6891703735494901b79d8c40dc453698601d442
Pisaric <116560852+Pisaric@users.noreply.github.com>
2024-04-30 13:24:29 +0200

    init component

--- src/components/CofeeManager/manager.module.ts
+++ src/components/CofeeManager/manager.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { CreateEventComponent } from './create-event/create-event.component'
 import { ManagerLandingPageComponent } from './manager-landing-page/manager-landing-page.component'
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { SupplyCreateComponent } from './supply-create/supply-create.component'
 
 @NgModule({
-    declarations: [CreateEventComponent, ManagerLandingPageComponent],
+    declarations: [CreateEventComponent, ManagerLandingPageComponent, SupplyCreateComponent],
     imports: [CommonModule, FormsModule],
     exports: [CreateEventComponent, ManagerLandingPageComponent],
 })
---
+++ src/components/CofeeManager/supply-create/supply-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supply-create',
+  templateUrl: './supply-create.component.html',
+  styleUrl: './supply-create.component.scss'
+})
+export class SupplyCreateComponent {
+
+}
