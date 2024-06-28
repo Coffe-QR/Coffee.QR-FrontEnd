7f0f4cf0e2fe1299dd2271180a5fcc1f91561630
z1 <miljanlazic@gmail.com>
2024-06-28 18:45:39 +0200

    FEAT: Calculating price and more design changes

--- src/components/Xuniversal/ticket-seat-select/ticket-seat-select.component.ts
+++ src/components/Xuniversal/ticket-seat-select/ticket-seat-select.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { ChartRendererConfigOptions } from '@seatsio/seatsio-types'
 import { EmbeddableProps } from '@seatsio/seatsio-angular'
+import { Router } from '@angular/router' // Import the Router class
 
 @Component({
     selector: 'app-ticket-seat-select',
@@ -8,17 +9,37 @@ import { EmbeddableProps } from '@seatsio/seatsio-angular'
     styleUrls: ['./ticket-seat-select.component.scss'],
 })
 export class TicketSeatSelectComponent implements OnInit {
-    config: EmbeddableProps<ChartRendererConfigOptions> = {
-        region: 'eu',
-        workspaceKey: '2d3804d9-bcc2-44cd-b613-b2e5afb398cb',
-        event: 'SAX-p1',
-        pricing: [
-            { category: 'Stolovi', price: 100 },
-            { category: 'Separei', price: 250 },
-        ],
-    }
+    totalpr: number = 0
 
-    constructor() {}
+    config: EmbeddableProps<ChartRendererConfigOptions> & { totalpr: number } =
+        {
+            region: 'eu',
+            workspaceKey: '2d3804d9-bcc2-44cd-b613-b2e5afb398cb',
+            event: 'SAX-p1',
+            pricing: [
+                { category: 'Stolovi', price: 100 },
+                { category: 'Separei', price: 250 },
+            ],
+            priceFormatter: (price) => '$' + price,
+            onObjectSelected: (object) => {
+                console.log(object.pricing.price)
+                this.totalpr += Number(object.pricing.price)
+            },
+
+            onChartRendered(chart) {
+                chart.changeConfig({
+                    filteredCategories: ['Stolovi', 'Separei'],
+                })
+                chart.zoomToFilteredCategories()
+            },
+            totalpr: this.totalpr,
+        }
+    constructor(private router: Router) {}
+
+    onContinueToPayment(): void {
+        alert('Total price: ' + this.totalpr)
+        this.router.navigate(['/payment'])
+    }
 
     ngOnInit(): void {}
 }
