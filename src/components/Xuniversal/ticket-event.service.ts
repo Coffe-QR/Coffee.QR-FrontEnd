233bb012279fbc76947dbf41267b8f8dc082b329
z1 <miljanlazic@gmail.com>
2024-07-07 21:01:52 +0200

    FEAT: Added ticket-event service

---
+++ src/components/Xuniversal/ticket-event.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { Ticket } from '../../auth/model/ticket.model'
+
+@Injectable({
+    providedIn: 'root',
+})
+export class TicketEventService {
+    private apiUrl = 'https://localhost:44333/api/cardevents'
+
+    constructor(private http: HttpClient) {}
+
+    createCardEvent(cardEventDto: any): Observable<any> {
+        return this.http.post(
+            `${this.apiUrl}`,
+            cardEventDto,
+            this.getHttpOptions()
+        )
+    }
+
+    deleteCardEvent(cardEventId: number): Observable<any> {
+        return this.http.delete(
+            `${this.apiUrl}/${cardEventId}`,
+            this.getHttpOptions()
+        )
+    }
+
+    getAllByEventId(eventId: number): Observable<any> {
+        return this.http.get(
+            `${this.apiUrl}/event/${eventId}`,
+            this.getHttpOptions()
+        )
+    }
+
+    getAllCardEvents(): Observable<any> {
+        return this.http.get(`${this.apiUrl}`, this.getHttpOptions())
+    }
+
+    getByIdAsync(id: number): Observable<any> {
+        return this.http.get(`${this.apiUrl}/${id}`, this.getHttpOptions())
+    }
+
+    private getHttpOptions() {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+            }),
+        }
+        return httpOptions
+    }
+}
