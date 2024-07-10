91d41c93d223a1cf7a75e9f98ea1bec0dd9a1076
z1 <miljanlazic@gmail.com>
2024-07-10 15:44:16 +0200

    FEAT: Redesign Tickets-Select component

--- src/components/Xuniversal/tickets-select/tickets-select.component.ts
+++ src/components/Xuniversal/tickets-select/tickets-select.component.ts
@@ -9,6 +9,7 @@ import { Router } from '@angular/router'
 import { TicketService } from '../ticket.service'
 import { Ticket } from '../../../auth/model/ticket.model'
 import { TicketUserService } from '../ticket-user.service'
+import { TicketEventService } from '../ticket-event.service'
 
 @Component({
     selector: 'app-tickets-select',
@@ -20,16 +21,19 @@ export class TicketsSelectComponent implements OnInit {
     eventId: number = 0
     local: Local | undefined
     userId: number = 0
-    tickets: Ticket[] = []
+    tickets: any[] = []
     selectedType: string = ''
     quantity: number = 1
     totalPrice: number = 0
+
+    cat1: any[] = []
 
     constructor(
         private eventService: EventService,
         private localService: LocalService,
         private authService: AuthService,
         private ticketService: TicketService,
+        private ticketEventService: TicketEventService,
         private ticketUserService: TicketUserService,
         private router: Router,
         private route: ActivatedRoute
@@ -51,57 +55,9 @@ export class TicketsSelectComponent implements OnInit {
                     })
             }
         })
-
-        this.ticketService
-            .getAllByEventId(this.eventId)
-            .subscribe((tickets) => {
-                this.tickets = tickets
-            })
     }
 
     onContinueToPayment(): void {
-        // if (this.userId === 0) {
-        //     console.error('User not logged in')
-        //     this.router.navigate(['/login'])
-        //     return
-        // } else {
-        //     const ticket = this.tickets.find(
-        //         (t) =>
-        //             +t.eventId === +this.eventId && t.type === this.selectedType
-        //     )
-
-        //     const ticketUser = {
-        //         cardId: ticket?.id || 0,
-        //         userId: this.userId,
-        //         quantity: this.quantity,
-        //         amount: this.totalPrice,
-        //         currency: 'usd',
-        //         paymentStatus: 'pending',
-        //         stripePaymentIntentId: '',
-        //         paymentMethod: 'card',
-        //     }
-        //     this.router.navigate(['/payment'], {
-        //         state: { ticketUser: ticketUser },
-        //     })
         this.router.navigate(['/ticket-seat-select/', this.eventId])
     }
 }
-
-// getTotalPrice(): void {
-//     if (!this.selectedType || !this.eventId) {
-//         this.totalPrice = 0
-//         return
-//     }
-
-//     const ticket = this.tickets.find(
-//         (t) => +t.eventId === +this.eventId && t.type === this.selectedType
-//     )
-
-//     if (ticket) {
-//         this.totalPrice = ticket.price * this.quantity
-//     } else {
-//         console.error('No ticket found for the selected type and event')
-//         this.totalPrice = 0
-//     }
-// }
-//}
