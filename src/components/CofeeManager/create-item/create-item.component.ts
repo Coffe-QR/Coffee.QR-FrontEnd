61a4d4ca156459383a865460d094f3cc839c843e
z1 <miljanlazic@gmail.com>
2024-09-18 16:43:51 +0200

    FEAT: Create more items

--- src/components/CofeeManager/create-item/create-item.component.ts
+++ src/components/CofeeManager/create-item/create-item.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core'
 import { Router } from '@angular/router'
 import { ItemService } from '../item.service'
 import { UploadService } from '../../../shared/upload.service'
+import { ToastrService } from 'ngx-toastr'
+import { NgForm } from '@angular/forms'
 
 @Component({
     selector: 'app-create-item',
@@ -9,10 +11,12 @@ import { UploadService } from '../../../shared/upload.service'
     styleUrls: ['./create-item.component.scss'],
 })
 export class CreateItemComponent implements OnInit {
+    @ViewChild('createItemForm') createItemForm!: NgForm // Reference the form
+    @ViewChild('fileInput') fileInput!: ElementRef // Reference the file input
+
     submitAttempted = false
-    //properties
+    // Properties
     itemName: string = ''
-    //itemType: any
     itemDescription: string = ''
     itemPrice: number = 0
     itemPicture: string = ''
@@ -20,7 +24,8 @@ export class CreateItemComponent implements OnInit {
     constructor(
         private uploadService: UploadService,
         private itemService: ItemService,
-        private router: Router
+        private router: Router,
+        private toastr: ToastrService
     ) {}
 
     ngOnInit(): void {}
@@ -28,22 +33,34 @@ export class CreateItemComponent implements OnInit {
     onSubmit(): void {
         const itemData = {
             name: this.itemName,
-            //type: Number(this.itemType),
             description: this.itemDescription,
             price: this.itemPrice,
             picture: this.itemPicture,
         }
 
         this.submitAttempted = true
+
         if (this.itemPrice <= 0) {
             return
         }
 
         this.itemService.createItem(itemData).subscribe({
             next: (response) => {
-                this.router.navigate(['/manager'])
+                this.toastr.success('Item created successfully', 'Success')
+                this.submitAttempted = false
+
+                // Reset the form
+                this.createItemForm.resetForm()
+
+                // Manually reset the file input
+                if (this.fileInput) {
+                    this.fileInput.nativeElement.value = '' // Clear the file input field
+                }
             },
-            error: (error) => console.error('Error creating item:', error),
+            error: (error) => {
+                console.error('Error creating item:', error)
+                this.submitAttempted = false
+            },
         })
     }
 
